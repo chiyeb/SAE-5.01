bcfa58e9e582a92ec962b2b986ad6576486b549a
ayoug <ayoub.essalhia@gmail.com>
2024-11-23 14:42:36 +0100

    refactor: added .env to avoid sharing personal data on git

--- src/main/java/com/example/saebackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/saebackend/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +14,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
-        http.csrf().disable();
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
